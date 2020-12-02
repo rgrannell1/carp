5025f3347c83ba1a4471373b91e20d3dd723b72b
Róisín Grannell <r.grannell2@gmail.com>
2020-12-02 22:23:05 +0000

    added file-test function

--- carp.go
+++ carp.go
@@ -44,10 +44,56 @@ type DependencyResult struct {
 	Met bool
 }
 
+// TestFileDependency checks a file exists
+func TestFileDependency(tgt Dependency) DependencyResult {
+	if tgt["path"] == "" {
+		return DependencyResult{Met: false}
+	}
+
+	info, err := os.Stat(tgt["path"])
+	if os.IsNotExist(err) {
+		return DependencyResult{Met: false}
+	}
+	if info.IsDir() {
+		return DependencyResult{Met: false}
+
+	}
+	return DependencyResult{Met: true}
+}
+
+// TestEnvVarDependency checks an environmental variable exists
+func TestEnvVarDependency(tgt Dependency) DependencyResult {
+	if tgt["name"] == "" {
+		return DependencyResult{Met: false}
+	}
+
+	val, present := os.LookupEnv(tgt["name"])
+
+	if !present {
+		return DependencyResult{Met: false}
+	}
+
+	if tgt["value"] != "" && val != tgt["value"] {
+		return DependencyResult{Met: false}
+	}
+
+	return DependencyResult{Met: true}
+}
+
 // TestDependency checks one dependency (or a carp-group) resolves as expected
 func TestDependency(tgt Dependency) DependencyResult {
 	switch id := tgt["id"]; {
+	case id == "core/file":
+		return TestFileDependency(tgt)
+	case id == "core/apt":
+		return TestEnvVarDependency(tgt)
+	case id == "core/folder":
+		return TestEnvVarDependency(tgt)
+	case id == "core/envvar":
+		return TestEnvVarDependency(tgt)
 	case id == "core/carpgroup":
+		return DependencyResult{Met: false}
+	case id == "core/snap":
 		return DependencyResult{Met: false}
 	default:
 		return DependencyResult{Met: false}
@@ -81,8 +127,6 @@ func carp(args CarpArgs) error {
 	if err != nil {
 		return err
 	}
-
-	// resolve all dependencies
 
 	tgt := carpfile[args.group]
 
