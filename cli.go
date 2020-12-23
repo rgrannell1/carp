f84ab17c9d850dd3ec44163e4786e24800d3a9b8
Róisín Grannell <r.grannell2@gmail.com>
2020-12-23 16:00:00 +0000

    added docs

--- cli.go
+++ cli.go
@@ -15,10 +15,53 @@ type CarpArgs struct {
 func main() {
 	usage := `Carp
 Usage:
-	carp --file <path> --group <name>
+	carp --file <path> [--group <name>]
+
+Description:
+	Carp is a simple dependency-checker that checks a host matches the expected configuration. Dependencies are specified
+	as a JSON file with groups of subdependencies. A minimal carpfile is shown below:
+
+	{
+		"folders": {
+			"requires": [
+				{
+					"id": "core/folder",
+					"path": "/home/myname/"
+				}
+			]
+		},
+		"vars": {
+			"requires": [
+				{
+					"id": "core/envvar",
+					"name": "SHELL",
+					"value": "/usr/bin/zsh"
+				}
+			]
+		},
+		"main": {
+			"requires": [
+				{
+					"id": "core/carpgroup",
+					"name": "vars"
+				},
+				{
+					"id": "core/carpgroup",
+					"name": "folders"
+				}
+			]
+		}
+	}
+
+	this example has three groups: main, vars, and folders. "vars" checks SHELL points to ZSH, and "folders" checks that
+	a home folder is setup for "myname". "main" in turn checks both the "vars" and "folder" groups meet their expected
+	state.
+
+	Writing JSON by hand can be repetitious, so carp can also read stdout if it's provided an executable file that prints JSON.
 
 Options:
-	--group <name> the group to test [default: main]
+	--group <name>    the group to test [default: main]
+	--file <path>     the path of the carpfile
 `
 
 	opts, _ := docopt.ParseDoc(usage)
