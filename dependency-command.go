4438915d7df51bc857ef42860f55cf0965f4aa8f
Róisín Grannell <r.grannell2@gmail.com>
2021-07-11 15:55:33 +0100

    implemented command check

--- dependency-command.go
+++ dependency-command.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"os/exec"
+)
+
 // Checks that a command is present on a system
 func TestCommand(tgt Dependency) (bool, []string) {
-	return false, []string{"a"}
+	if tgt["name"] == "" {
+		return false, []string{"group name not provided"}
+	}
+
+	cmd := exec.Command("which", tgt["name"])
+	runErr := cmd.Run()
+
+	if runErr != nil {
+		exitError := runErr.(*exec.ExitError)
+
+		if exitError != nil {
+			code := exitError.ExitCode()
+
+			if code != 0 {
+				return false, []string{tgt["name"] + " not present"}
+			}
+		}
+	}
+
+	return true, []string{tgt["name"] + " present"}
 }
